d28d3ed2d37ff7de4374f876b72c41fec399954e
ilhamaliyev-az <rjjfjdjdmkdkdkdk@gmail.com>
2026-07-09 22:44:04 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Cache-first strategy for full offline support
    ================================================ */
 
-var CACHE_NAME = 'zeytun-v3';
+var CACHE_NAME = 'zeytun-v5';
 var ASSETS = [
   './',
   './index.html',
